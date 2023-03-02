6d689b39ef19541d731b7c217d77362e8ab23830
Daniel Guest <guest121@umn.edu>
2023-03-02 14:49:48 -0500

    Removed model code from Helios
    Removed C model code from Helios, will be added back in as a
    subrepo in the next commit

--- src/model/compile.sh
+++
@@ -1,7 +0,0 @@
-# Step 1: Compile model.c, complex.c, with warnings (-Wall), position independent 
-# code (-fPIC), and optimization level three (-O3)
-gcc -c -Wall -fPIC -O3 complex.c 
-gcc -c -Wall -fPIC -O3 model.c
-
-# Step 2: Compile resulting objects together into a shared library 
-gcc -shared -o libgfc2023.so complex.o model.o
