5a6ff1bd9b68934e44a26008655920694ebe112b
Daniel Guest <guest121@umn.edu>
2023-08-07 15:34:18 -0400

    Slight improvements to sim_efferent_model.m
    Updated and cleaned up some of the help docs at the top of the file.
    
    Also added some validators to the arguments block to make sure that we
    are always passing row vectors and checking to make sure that parameters
    with discrete numbers of options (e.g., noiseType) are checked to make
    sure that the option is legitimate.

--- src/mex/sim_efferent_model.m
+++ src/mex/sim_efferent_model.m
@@ -6,89 +6,105 @@ function [ihcout, hsrout, lsrout, icout, gain] = sim_efferent_model(x, cf, args)
 %   - `n_chan=length(cf)`
 % 
 % Returned values are (in order): 
-%   1) Inner hair cell "voltage"
-%   2) High-spontaneous-rate auditory-nerve instantaneous rate
-%   3) Low-spontaneous-rate auditory-nerve instantaneous rate
-%   4) Inferior-colliculus (IC) band-enhanced rate
+%   1) Inner hair cell "voltage", in a.u.
+%   2) High-spontaneous-rate auditory-nerve instantaneous rate, in sp/s
+%   3) Low-spontaneous-rate auditory-nerve instantaneous rate, in sp/s
+%   4) Inferior-colliculus (IC) band-enhanced rate, in sp/s
 %   5) Time-varying cochlear gain factor (in [0, 1], where 0==no gain, 1==max gain)
 %
-% SIM_EFFERENT_MODEL(x, cf) passes inputs, after some minor calculations 
-% and preprocessing, to `sim_efferent_model_mex`, a Mex wrapper to the C
-% implementation of the efferent model. 
+% SIM_EFFERENT_MODEL(x, cf) passes evaluates the efferent model on the
+% input sound-pressure waveform at particular CFs. 
 %
-% SIM_EFFERENT_MODEL(x, cf, species=2) runs the simulation for a 
+% SIM_EFFERENT_MODEL(x, cf, species=2) runs the efferent model for a 
 % species value of 2 (which corresponds to human tuning based on data from 
 % Shera). Other model parameters, such as sampling rate or IC model
-% parameters are adjusted the same way by specifying a key-value
-% combination (e.g., ic_tau_e=0.5e-3 would set the inhibitory IC delay to
+% parameters, are adjusted the same way by specifying a key-value
+% combinations (e.g., ic_tau_e=0.5e-3 would set the inhibitory IC delay to
 % 0.5 ms, or moc_cutoff=2.0 would set the MOC lowpass cutoff to 2 Hz). See
 % below for more details about available parameters and their default
-% values.
+% values (which are always used unless otherwise specified).
 %
 % Arguments:
-% - x: Row vector containing input sound-pressure waveform
-% - cf: Row vector containing characteristic frequencies for each channel 
+% - x: Vector containing input sound-pressure waveform (Pa)
+%
+% - cf: Vector containing characteristic frequencies for each channel 
 %   in the simulation (Hz)
-% - args.fs: Sampling rate (Hz)
-% - args.cohc: Outer-hair-cell "count/health" (in [0, 1])
-% - args.cihc: Inner-hair-cell "count/health" (in [0, 1])
+%
+% - args.fs: Sampling rate of the simulation (Hz). Note that inputs must be 
+%   sampled at this sampling rate. 
+%
+% - args.cohc: Outer-hair-cell "count/health" (in [0, 1], where 0==no 
+%   contribution of OHCs, 1==maximum contribution of OHCs)
+%
+% - args.cihc: Inner-hair-cell "count/health" (in [0, 1], where 0==no IHC 
+%   amplitude, 1==normal IHC amplitude)
+%
 % - args.species: Which species to simulate in the basilar membrane/inner 
-%   hair cell stage, 1==cat, 2==human[Shera], 3==human[Glasberg]
+%   hair cell stage, 1==cat, 2==human (Shera), 3== (Moore and Glasberg)
+%
 % - args.powerlaw_mode: Whether to use true power-law adaptation
 %   (powerlaw_mode == 1) or an approximate power-law implementation using a
 %   set of 100 parallel exponential adaptation processes with time
 %   constants fit computationally to match true power-law adaptation
-%   (powerlaw_mode == 2). The approximation is not comprehensively tested,
-%   but appears to be very good for short responses (< 1 s) at moderate CFs
-%   (<= ~3-4 kHz). The `demo.m` script bundled with this code includes a
-%   demo validation of the approximation in response to pure tones at
-%   various frequencies.
+%   (powerlaw_mode == 2). 
+%
 % - args.ic_tau_e: Excitatory time constant in IC stage (s)
+%
 % - args.ic_tau_i: Inhibitory time constant in IC stage (s)
+%
 % - args.ic_delay: Inhibitory delay time in IC stage (s)
+%
 % - args.ic_amp: Excitatory strength in IC stage
+%
 % - args.ic_inh: Inhibitory strength in IC stage 
+%
 % - args.moc_cutoff: Cutoff of the lowpass filter used in the MOC stage
 %   (Hz). The default value of 0.64 Hz yields a filter that matches that
 %   used in the older single-channel efferent model (i.e., it produces a
 %   "decay constant" of exp(-2pi * 0.64/100e3) ~= 1-3.9998e-5, which 
 %   matches the constant used in the "old efferent" code, see Farhadi et 
 %   al. 2023).
+%
 % - args.moc_beta_wdr: "beta" parameter in the MOC input-output
 %   nonlinearity for the wide-dynamic-range MOC pathway (a.u.)
+%
 % - args.moc_offset_wdr: "offset" parameter in the MOC input-output
 %   nonlinearity for the wide-dynamic-range MOC pathway (a.u.)
+%
 % - args.moc_beta_ic: "beta" parameter in the MOC input-output
 %   nonlinearity for the IC MOC pathway (a.u.)
+%
 % - args.moc_offset_ic: "offset" parameter in the MOC input-output
 %   nonlinearity for the IC MOC pathway (a.u.)
+%
 % - args.moc_weight_wdr: Scalar value multiplied with lowpass-filtered 
 %   wide-dynamic-range MOC pathway signal before signal is passed through 
 %   MOC input-output nonlinearity
+%
 % - args.moc_weight_ic: Scalar value multiplied with lowpass-filtered IC
 %   MOC pathway signal before signal is passed through MOC input-output 
 %   nonlinearity
+%
 % - args.moc_width_wdr: "Width" of the wide-dynamic-range cross-channel
 %   "spread" (octaves). For example, a value of one octave means that each
 %   wide-dynamic-range MOC signal will "spread" to all channels that have
 %   CFs that fall within a band centered on the CF with a width of one
 %   octave (within +/- one-half octave)
+%
 % - args.noiseType: Integer value determining whether we use empty matrices
 %   (noiseType == -1), matrices of "frozen" fractional Gaussian noise 
 %   (noiseType == 0), or matrices of "fresh" fractional Gaussian noise based
 %   on the current global RNG state (noiseType == 1) as inputs for the
 %   noise governing the stochastic behavior of the power-law synapse in the
 %   auditory-nerve model. 
-
-% Set arguments and defaults
 arguments
     x (1,:)
-    cf (1,:)
-    args.fs = 100e3
-    args.cohc = 1.0
-    args.cihc = 1.0
-    args.species = 1 
-	args.powerlaw_mode = 2
+    cf (1,:) {mustBeGreaterThanOrEqual(cf, 125.0), mustBeLessThanOrEqual(cf, 40e3)}
+    args.fs {mustBeGreaterThanOrEqual(args.fs,50e3), mustBeLessThanOrEqual(args.fs,200e3)} = 100e3
+    args.cohc {mustBeGreaterThanOrEqual(args.cohc, 0.0), mustBeLessThanOrEqual(args.cohc, 1.0)} = 1.0
+    args.cihc {mustBeGreaterThanOrEqual(args.cihc, 0.0), mustBeLessThanOrEqual(args.cihc, 1.0)} = 1.0
+    args.species {mustBeMember(args.species, [1, 2, 3])} = 1 
+	args.powerlaw_mode {mustBeMember(args.powerlaw_mode, [1, 2])} = 2
     args.ic_tau_e = 1e-3
     args.ic_tau_i = 2e-3
     args.ic_delay = 1e-3
@@ -102,7 +118,7 @@ arguments
     args.moc_weight_wdr = 2.0
     args.moc_weight_ic = 8.0
     args.moc_width_wdr = 0.5
-    args.noiseType = 1
+    args.noiseType {mustBeMember(args.noiseType, [-1, 0, 1])} = 1
 end
 
 % Determine number of channels and samples
